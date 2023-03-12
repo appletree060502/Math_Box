f446817dad560d338a2a7486eaf443ed851eb06f
appletree060502 <127353130+appletree060502@users.noreply.github.com>
2023-03-12 16:04:17 +0900

    Add say_hello function

--- calculator.py
+++ calculator.py
@@ -1,16 +1,15 @@
  # 기본계산기
 def add(a, b)
-	return a+b
+    return a+b
 
 def subtract(a,b)
- 	return a-b	
+    return a-b	
 
 def multiply(a,b):
-	return a*b 	
+    return a*b 	
 
-<<<<<<< HEAD
-def divide_premium(a, b):  
-=======
-def divide_free(a, b):  
->>>>>>> main
+def divide_new(a, b):  
     return a/b
+
+def say_hello():
+    print("hello")	
